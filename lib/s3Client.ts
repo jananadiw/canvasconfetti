91fe1d8cf8e90333066537382c682d9afe0d669f
Jananadi Wedagedara <jananadi92@gmail.com>
2023-02-05 23:57:25 +0900

    feat: Implement S3 service client

---
+++ lib/s3Client.ts
@@ -0,0 +1,10 @@
+import { S3Client } from '@aws-sdk/client-s3'
+// Set the AWS Region.
+const REGION = 'ap-northeast-2'
+const CREDENTIAL = {
+  accessKeyId: `${process.env.AWS_ACCESS_KEY_ID}`,
+  secretAccessKey: `${process.env.AWS_SECRET_ACCESS_KEY}`
+}
+// Create an Amazon S3 service client object.
+const s3Client = new S3Client({ region: REGION, credentials: CREDENTIAL })
+export { s3Client }
--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  reactStrictMode: true,
+  images: { 
+    remotePatterns: [
+    {
+      protocol: 'https',
+      hostname: process.env.ARTWORK_HOSTNAME_URL,
+      
+    },
+  ],
+}
 }
 
 module.exports = nextConfig
