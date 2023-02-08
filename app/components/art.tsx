3d59756ea455a9460dc0a3811737b09a069022b2
Jananadi Wedagedara <jananadi92@gmail.com>
2023-02-09 00:59:28 +0900

    style: Update title text, padding

--- app/components/art.tsx
+++ app/components/art.tsx
@@ -8,9 +8,9 @@ export default function Art({data}: any) {
         <Image
         src={`https://${process.env.AWS_BUCKET}.s3.ap-northeast-2.amazonaws.com/${key}`}
         alt={`img_${key}`}
-        width={400}
-        className="w-full"
-        height={400}
+        width={500}
+        className="w-full rounded"
+        height={600}
         />
     </main>
   )
