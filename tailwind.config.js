f6b0353ca83f601d2b7eccbf260ed1e35756ff96
Jananadi Wedagedara <jananadi92@gmail.com>
2023-02-12 23:52:43 +0900

    style: Add custom font styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'raleway' : ['Raleway','sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
