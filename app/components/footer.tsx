262a4be45d2b9d8514ade3518840bf7bba973442
Jananadi Wedagedara <jananadi92@gmail.com>
2023-02-14 02:27:33 +0900

    style: Fix footer height

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -3,11 +3,9 @@ import "@/styles/globals.scss";
 
 export default function Footer() {
 return(
-    <main>
-        <div className="mx-auto w-full fixed bottom-0 h-8 bg-white text-black font-raleway text-center justify-content py-10 text-lg">
+        <div className="w-full fixed bottom-0 bg-white text-black font-raleway text-center justify-content p-6 text-lg">
             <p>All Rights Reserved &copy;Canvas Confetti</p>
         </div>
-    </main>
 )
 
 }
