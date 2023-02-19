c919ac7e225f7383060ba756d7f5370b2fedf8c7
Jananadi Wedagedara <jananadi92@gmail.com>
2023-02-20 02:41:19 +0900

    style: [wip]Update overlay of the artwork detail

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -9,23 +9,21 @@ export default function Page(){
     return (
       <>
         <main>
-            <div className="flex flex-row mx-80 h-full px-10">
-            <div className="basis-1/2 ml-24">
+            <div className="flex flex-col mx-80 px-10 justify-center items-center">
+            <div className="text-l">
+                {/* TODO: Fetch details from a database */}
+                <p>Oh Coffee • Portrait • Digital Art • A4 • $15.00</p>
+              </div>
+            <div className="mt-10">
             <Image
             // FIXME: Fetch bucket name from .env file
               src={`https://janawcontent.s3.ap-northeast-2.amazonaws.com${pathname}`}
               alt={`img_${pathname}`}
-              width={400}
-              height={500}
+              width={550}
+              height={600}
             />
             </div>
-            <div className="basis-1/2 text-left self-end font-raleway px-10 leading-loose font-semibold">
-              <div className="inset-x-0 top-0">
-                {/* TODO: Fetch details from a database */}
-                <p>Name: Oh Coffee</p>
-                <p>Medium: Digital Art</p>
-                <p>Size: A4</p>
-              </div>
+            <div className="basis-1/2 text-left self-end font-raleway px-10 leading-loose font-semibold mb-20">
               <div className="inset-x-0 bottom-0">
                 <Link href={{pathname: '/'}}>
                 <button className="bg-transparent text-blue-700 font-semibold  hover:text-gray-400"> View All </button>
