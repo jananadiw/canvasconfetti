3564d01246b28c7b15acbff2d4fbc7310ba0d367
Jananadi Wedagedara <jananadi92@gmail.com>
2023-02-20 02:39:51 +0900

    style: Revert nav bar style

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -8,7 +8,7 @@ import "@/styles/globals.scss";
 export default function NavBar() {
   return (
     <>
-      <div className="flex justify-center items-center w-96 mb-24 font-raleway mx-auto my-28">
+      {/* <div className="flex justify-center items-center w-96 mb-24 font-raleway mx-auto my-28">
         <div className="flex-none float-right">
               <a href="//opensea.io/collection/canvas-confetti" target="_blank" rel="noreferrer">
                 <p className="text-gray-400 hover:text-gray-900">Shop</p>
@@ -24,6 +24,25 @@ export default function NavBar() {
               <p className="text-gray-400 hover:text-gray-900">About</p>
             </Link>
           </div>
+      </div> */}
+       <div className="flex flex-col mx-auto justify-center items-center w-full my-16 font-raleway">
+        <div className="cursor-pointer">
+        <Link href="/">
+          <Image src={ccLogo} alt="logo_cc" width={300} height={100} />
+        </Link>
+        </div>
+        <div className="uppercase mt-2 mx-auto">
+          <div className="flex flex-wrap justify-between items-center">
+            <div className="w-1/5 text-orange-300 hover:text-gray-900">
+              <a href="//opensea.io/collection/canvas-confetti" target="_blank" rel="noreferrer">
+                Shop
+              </a>
+            </div>
+            <Link href="/about" className="w-1/5">
+              <p className="text-orange-300 hover:text-gray-900">About</p>
+            </Link>
+          </div>
+        </div>
       </div>
     </>
   );
