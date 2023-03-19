8271bd1b49d4e338d255c91dd2de4ea353d13e1c
Jananadi Wedagedara <jananadi92@gmail.com>
2023-03-19 23:58:47 +0900

    style: navbar layout

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -5,44 +5,37 @@ import { useEffect, useState } from 'react';
 import ccLogo from 'public/cc_logo.png';
 import Image from 'next/image';
 import '@/styles/globals.scss';
+import { useScrollPosition } from '@/hooks/useScrollPosition';
 
 export default function NavBar() {
 
-const [isFixed, setIsFixed] = useState(false);
+  const scrollPosition = useScrollPosition();
 
-useEffect(() => {
-  const handleScroll = () => {
-    const threshold = (document.querySelector('.navbar') as HTMLElement).offsetTop;
-    setIsFixed(window.pageYOffset >= threshold);
-  };
-
-  window.addEventListener('scroll', handleScroll);
-
-  return () => {
-    window.removeEventListener('scroll', handleScroll);
-  };
-}, []);
-
+  const classNames = (...classes: any) => {
+    return classes.filter(Boolean).join(' ');
+  }
   return (
     <>
-      {/* <div className="flex justify-center items-center w-96 mb-24 font-raleway mx-auto my-28">
-        <div className="flex-none float-right">
+    {/* <div className={classNames(scrollPosition > 0) ? 'bg-white sticky top-0 z-10 transition-all': 'hidden'}>
+      <div className='flex justify-center items-center w-96 mb-24 font-raleway mx-auto my-16 p-12' >
+        <div className="flex-none float-right bg-white">
               <a href="//opensea.io/collection/canvas-confetti" target="_blank" rel="noreferrer">
-                <p className="text-gray-400 hover:text-gray-900">Shop</p>
+                <p className="text-gray-900 hover:text-gray-300">Shop</p>
               </a>
             </div>
-        <div className="cursor-pointer flex-none mx-32">
+        <div className="cursor-pointer flex-none mx-32 bg-white">
         <Link href="/">
           <Image src={ccLogo} alt="logo_cc" width={450} height={100} />
         </Link>
         </div>
-        <div className="flex-1">
+        <div className="flex-1 bg-white">
             <Link href="/about" className="">
-              <p className="text-gray-400 hover:text-gray-900">About</p>
+              <p className="text-gray-900 hover:text-gray-300">About</p>
             </Link>
           </div>
+      </div>
       </div> */}
-      <div className="flex flex-col mx-auto justify-center items-center w-full mt-16 mb-28 font-raleway">
+      <div className='flex flex-col mx-auto justify-center items-center w-full my-16 p-12 font-raleway bg-white sticky top-0 z-10'>
         <div className="cursor-pointer">
           <Link href="/">
             <Image src={ccLogo} alt="logo_cc" width={400} height={100} />
